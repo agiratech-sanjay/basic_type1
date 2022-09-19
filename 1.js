662ccd4d74be33e4213ff3f07f6e722682ed23ea
agiratech-sanjay <sanjay.k@agiratech.com>
2022-09-19 19:21:06 +0530

    first1 commit

--- 1.js
+++ 1.js
@@ -1,1 +1,15 @@
-console.log("hello");
+"use strict";
+exports.__esModule = true;
+var animal = /** @class */ (function () {
+    function animal() {
+    }
+    animal.prototype.run = function () {
+        console.log("hakuna matata");
+    };
+    return animal;
+}());
+exports.animal = animal;
+var tiger = new animal();
+tiger.health = 50;
+tiger.speed = 100;
+tiger.run();
--- 1.ts
+++ 1.ts
@@ -1,1 +1,14 @@
-console.log("hello")
+export class animal{
+    health:number
+    speed:number
+
+    run(){
+        console.log("hakuna matata");
+    }
+ 
+}
+
+const tiger = new animal()
+tiger.health = 50;
+tiger.speed = 100;
+tiger.run()
